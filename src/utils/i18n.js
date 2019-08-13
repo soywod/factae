724ff5e3a2d1e0a521b48b475b57dd1da4af4368
Clément DOUIN <soywod@users.noreply.github.com>
2019-08-13 13:14:22 +0200

    add MIT license

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -9,7 +9,7 @@ i18n
   .use(initReactI18next)
   .init({
     load: 'languageOnly',
-    fallbackLng: 'en',
+    fallbackLng: 'fr',
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
       escapeValue: false,
