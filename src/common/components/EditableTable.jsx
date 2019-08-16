3badeddc90cc450382ce4d7677c6ccf14da4a3ee
Clément DOUIN <soywod@users.noreply.github.com>
2019-08-16 14:39:38 +0200

    improve input number items form

--- src/common/components/EditableTable.jsx
+++ src/common/components/EditableTable.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import Table from 'antd/es/table'
 import Input from 'antd/es/input'
+import InputNumer from 'antd/es/input-number'
 import Form from 'antd/es/form'
+import isNumber from 'lodash/fp/isNumber'
+import range from 'lodash/fp/range'
+
+const BACK_KEY_CODE = 8
+const ENTER_KEY_CODE = 13
+const DOT_KEY_CODE = 190
+const NUMBER_KEY_CODES = range(48, 58)
+const KEYPAD_KEY_CODES = range(96, 106)
+const ARROWS_KEY_CODES = range(37, 41)
+const INPUT_NUMBER_VALID_KEY_CODES = [
+  BACK_KEY_CODE,
+  DOT_KEY_CODE,
+  ...ARROWS_KEY_CODES,
+  ...NUMBER_KEY_CODES,
+  ...KEYPAD_KEY_CODES,
+]
 
 const EditableContext = React.createContext()
 
@@ -27,10 +44,19 @@ class EditableCell extends React.Component {
     })
   }
 
-  save = e => {
+  handleKeyDown = event => {
+    if (event.keyCode === ENTER_KEY_CODE) {
+      event.stopPropagation()
+      event.currentTarget.blur()
+    } else if (!INPUT_NUMBER_VALID_KEY_CODES.includes(event.keyCode)) {
+      event.preventDefault()
+    }
+  }
+
+  save = event => {
     const {record, handleSave} = this.props
     this.form.validateFields((error, values) => {
-      if (error && error[e.currentTarget.id]) {
+      if (error && error[event.currentTarget.id]) {
         return
       }
       this.toggleEdit()
@@ -47,12 +73,25 @@ class EditableCell extends React.Component {
         {form.getFieldDecorator(dataIndex, {
           initialValue: record[dataIndex],
         })(
-          <Input
-            ref={node => (this.input = node)}
-            onPressEnter={this.save}
-            onBlur={this.save}
-            style={{width: '100%', height: 40}}
-          />,
+          isNumber(record[dataIndex]) ? (
+            <InputNumer
+              size="large"
+              ref={node => (this.input = node)}
+              min={0}
+              step={1}
+              onKeyDown={this.handleKeyDown}
+              onBlur={this.save}
+              style={{width: '100%', height: 40}}
+            />
+          ) : (
+            <Input
+              size="large"
+              ref={node => (this.input = node)}
+              onPressEnter={this.save}
+              onBlur={this.save}
+              style={{width: '100%', height: 40}}
+            />
+          ),
         )}
       </Form.Item>
     ) : (
