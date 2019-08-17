88cfd71ba5e36ed846988c5d094d76a6cfa6e95a
Clément DOUIN <soywod@users.noreply.github.com>
2019-08-17 20:01:18 +0200

    add client-side pagination for clients and docs

--- src/client/components/List.jsx
+++ src/client/components/List.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useTranslation} from 'react-i18next'
 import Button from 'antd/es/button'
 import Form from 'antd/es/form'
@@ -6,7 +6,6 @@ import Icon from 'antd/es/icon'
 import Table from 'antd/es/table'
 import omit from 'lodash/fp/omit'
 
-import ActionBar from '../../common/components/ActionBar'
 import Container from '../../common/components/Container'
 import {useNotification} from '../../utils/notification'
 import {useClients} from '../hooks'
@@ -15,8 +14,16 @@ import $client from '../service'
 function ClientList(props) {
   const clients = useClients()
   const [loading, setLoading] = useState(false)
+  const [pagination, setPagination] = useState({})
+
   const tryAndNotify = useNotification()
   const {t} = useTranslation()
+
+  useEffect(() => {
+    if (clients) {
+      setPagination({...pagination, total: clients.length})
+    }
+  }, [clients])
 
   if (!clients) {
     return null
@@ -27,7 +34,7 @@ function ClientList(props) {
       title: <strong>{t('name')}</strong>,
       dataIndex: 'name',
       key: 'name',
-      width: '40%',
+      width: '35%',
     },
     {
       title: <strong>{t('email')}</strong>,
@@ -40,6 +47,22 @@ function ClientList(props) {
       dataIndex: 'phone',
       key: 'phone',
       width: '30%',
+    },
+    {
+      title: (
+        <Button type="primary" shape="circle" onClick={createClient}>
+          <Icon type="plus" />
+        </Button>
+      ),
+      dataIndex: 'action',
+      key: 'action',
+      align: 'center',
+      width: '5%',
+      render: () => (
+        <Button type="link" size="small" shape="circle">
+          <Icon type="edit" />
+        </Button>
+      ),
     },
   ]
 
@@ -61,24 +84,17 @@ function ClientList(props) {
 
       <Table
         bordered
+        pagination={pagination}
         loading={loading}
         dataSource={clients.map(client => ({...client, key: client.id}))}
         columns={columns}
-        pagination={false}
         rowKey={record => record.id}
         onRow={record => ({
           onClick: () => props.history.push(`/clients/${record.id}`, {...omit('key', record)}),
         })}
-        style={{background: '#ffffff', marginBottom: 15}}
+        style={{background: '#ffffff'}}
         bodyStyle={{cursor: 'pointer'}}
       />
-
-      <ActionBar>
-        <Button type="primary" disabled={loading} onClick={createClient}>
-          <Icon type={loading ? 'loading' : 'plus'} />
-          {t('new')}
-        </Button>
-      </ActionBar>
     </Container>
   )
 }
