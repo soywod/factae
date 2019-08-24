03d67e475a0564ed085eac641ee80b451ea621df
Clément DOUIN <soywod@users.noreply.github.com>
2019-08-24 19:06:03 +0200

    add prorata temporis for new freelances

--- src/dashboard/hooks.js
+++ src/dashboard/hooks.js
@@ -1,3 +1,5 @@
+import {DateTime} from 'luxon'
+
 import {useProfile} from '../profile/hooks'
 
 export function useThresholds() {
@@ -7,12 +9,37 @@ export function useThresholds() {
     return [0, 0, 0]
   }
 
+  let ratio = 1
+  const now = DateTime.local()
+  const activityStartedAt = DateTime.fromISO(profile.activityStartedAt)
+
+  if (activityStartedAt.year === now.year) {
+    const firstDayOfYear = now.set({
+      month: 1,
+      day: 1,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: 0,
+    })
+
+    const lastDayOfYear = firstDayOfYear
+      .plus({year: 1})
+      .minus({day: 1})
+      .set({hour: 23, minute: 59, second: 59, millisecond: 999})
+
+    const yearDiff = lastDayOfYear - firstDayOfYear
+    const activityDiff = activityStartedAt - firstDayOfYear
+
+    ratio = activityDiff / yearDiff
+  }
+
   switch (profile.activity) {
     case 'trade':
-      return [82800, 91000, 170000]
+      return [82800, 91000, 170000].map(x => x * ratio)
 
     case 'service':
-      return [33200, 35200, 70000]
+      return [33200, 35200, 70000].map(x => x * ratio)
 
     default:
       return [0, 0, 0]
