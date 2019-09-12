051a87cc8598c0f6b58267e6b94b94aaccf227c6
Clément DOUIN <soywod@users.noreply.github.com>
2019-09-12 13:20:27 +0200

    add contacts to client

--- src/client/components/Edit.jsx
+++ src/client/components/Edit.jsx
@@ -1,34 +1,104 @@
 import React, {useEffect, useState} from 'react'
 import {useTranslation} from 'react-i18next'
 import Button from 'antd/es/button'
+import Card from 'antd/es/card'
+import Col from 'antd/es/col'
 import Form from 'antd/es/form'
 import Icon from 'antd/es/icon'
 import Input from 'antd/es/input'
 import Popconfirm from 'antd/es/popconfirm'
+import Row from 'antd/es/row'
+import Typography from 'antd/es/typography'
 import find from 'lodash/fp/find'
 
+import EditableTable from '../../common/components/EditableTable'
 import Title from '../../common/components/Title'
-import FormCard, {FormCardTitle, validateFields} from '../../common/components/FormCard'
+import {validateFields} from '../../common/components/FormCard'
+import FormItems from '../../common/components/FormItems'
 import Container from '../../common/components/Container'
 import {useNotification} from '../../utils/notification'
 import {useClients} from '../hooks'
 import $client from '../service'
+
+const formItemLayout = {
+  labelCol: {
+    xs: {span: 24},
+    sm: {span: 24},
+    md: {span: 24},
+    lg: {span: 5},
+  },
+  wrapperCol: {
+    xs: {span: 24},
+    sm: {span: 24},
+    md: {span: 24},
+    lg: {span: 19},
+  },
+}
+
+const styles = {
+  title: {
+    fontSize: '1.2rem',
+    margin: '0 0 15px 0',
+  },
+}
 
 function EditClient(props) {
   const {form} = props
   const clients = useClients()
   const [loading, setLoading] = useState(false)
   const [client, setClient] = useState(props.location.state)
+  const [contacts, setContacts] = useState(client.contacts || [])
   const tryAndNotify = useNotification()
   const {t} = useTranslation()
   const requiredRules = {rules: [{required: true, message: t('field-required')}]}
-  const emailRules = {rules: [{type: 'email', message: t('email-invalid')}]}
 
   useEffect(() => {
     if (clients && !client) {
-      setClient(find({id: props.match.params.id}, clients))
+      const nextClient = find({id: props.match.params.id}, clients)
+      setClient(nextClient || {})
+      setContacts(nextClient.contacts || [])
     }
   }, [client, clients, props.match.params.id])
+
+  const columns = [
+    {
+      title: <strong style={{marginLeft: 16}}>{t('name')}</strong>,
+      dataIndex: 'name',
+      key: 'name',
+      editable: true,
+      width: '34%',
+    },
+    {
+      title: <strong>{t('email')}</strong>,
+      dataIndex: 'email',
+      key: 'email',
+      editable: true,
+      width: '33%',
+    },
+    {
+      title: <strong>{t('phone')}</strong>,
+      dataIndex: 'phone',
+      key: 'phone',
+      editable: true,
+      width: '33%',
+    },
+    {
+      title: (
+        <Button type="primary" shape="circle" onClick={addContact}>
+          <Icon type="plus" />
+        </Button>
+      ),
+      dataIndex: 'action',
+      key: 'action',
+      fixed: 'right',
+      align: 'right',
+      render: (_, {key}) => (
+        <Button type="danger" shape="circle" onClick={() => removeContact(key)}>
+          <Icon type="minus" />
+        </Button>
+      ),
+    },
+  ]
 
   async function deleteClient() {
     await tryAndNotify(
@@ -50,6 +120,7 @@ function EditClient(props) {
     await tryAndNotify(async () => {
       let nextClient = await validateFields(form)
       nextClient.id = client.id
+      nextClient.contacts = contacts
       setClient(nextClient)
       await $client.set(nextClient)
       return t('/clients.updated-successfully')
@@ -58,35 +129,49 @@ function EditClient(props) {
     setLoading(false)
   }
 
+  function addContact() {
+    setContacts([
+      ...contacts,
+      {
+        key: Date.now(),
+        name: '',
+        email: '',
+        phone: '',
+      },
+    ])
+  }
+
+  function saveContacts(nextContact) {
+    const prevContactIndex = contacts.findIndex(item => nextContact.key === item.key)
+    const prevContact = contacts[prevContactIndex]
+
+    contacts.splice(prevContactIndex, 1, {
+      ...prevContact,
+      ...nextContact,
+    })
+
+    setContacts([...contacts])
+  }
+
+  function removeContact(key) {
+    setContacts(contacts.filter(item => item.key !== key))
+  }
+
   if (!clients || !client) {
     return null
   }
 
-  const companyFields = {
-    title: <FormCardTitle title="company" />,
-    fields: [
-      {name: 'name', Component: <Input size="large" autoFocus />, ...requiredRules},
-      {name: 'siret'},
-      {name: 'email', ...emailRules},
-      {name: 'phone'},
-    ],
-  }
-
-  const addressFields = {
-    title: <FormCardTitle title="address" />,
-    fields: [
-      {name: 'address', ...requiredRules},
-      {name: 'zip', ...requiredRules},
-      {name: 'city', ...requiredRules},
-      {name: 'country', ...requiredRules},
-    ],
-  }
-
-  const fields = [companyFields, addressFields]
+  const companyFields = [
+    {name: 'name', Component: <Input size="large" autoFocus />, ...requiredRules},
+    {name: 'address', ...requiredRules},
+    {name: 'zip', ...requiredRules},
+    {name: 'city', ...requiredRules},
+    {name: 'country', ...requiredRules},
+  ]
 
   return (
     <Container>
-      <Form noValidate layout="vertical" onSubmit={saveClient}>
+      <Form noValidate {...formItemLayout} onSubmit={saveClient}>
         <Title label="client">
           <Button.Group>
             <Popconfirm
@@ -107,9 +192,29 @@ function EditClient(props) {
           </Button.Group>
         </Title>
 
-        {fields.map((props, key) => (
-          <FormCard key={key} form={form} model={client} {...props} />
-        ))}
+        <Card>
+          <Row gutter={24}>
+            <Col xs={24} sm={24} md={24} lg={12}>
+              <Typography.Title level={2} style={styles.title}>
+                {t('client-identity')}
+              </Typography.Title>
+              <FormItems form={form} model={client} fields={companyFields} />
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={12}>
+              <Typography.Title level={2} style={styles.title}>
+                {t('contacts')}
+              </Typography.Title>
+              <EditableTable
+                size="small"
+                pagination={false}
+                bodyStyle={{margin: 0}}
+                dataSource={contacts}
+                columns={columns}
+                onSave={saveContacts}
+              />
+            </Col>
+          </Row>
+        </Card>
       </Form>
     </Container>
   )
