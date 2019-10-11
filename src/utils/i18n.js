30273126e99aa42c732a3eb275a374e0b43d62f8
Clément DOUIN <soywod@users.noreply.github.com>
2019-10-11 18:15:23 +0200

    set french language by default

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,13 +1,12 @@
 import i18n from 'i18next'
 import Backend from 'i18next-xhr-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import {initReactI18next} from 'react-i18next'
 
 i18n
   .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: 'fr',
     load: 'currentOnly',
     whitelist: ['fr', 'en'],
     fallbackLng: 'fr',
